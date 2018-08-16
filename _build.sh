c841b2006b005fff70f4025e039bab6a874ffc28
Loïc Séguin-C <lsc@loicseguin.com>
2018-08-16 10:19:39 -0400

    Simplify build procedure

--- _build.sh
+++ _build.sh
@@ -6,8 +6,5 @@ make
 make clean
 cd ..
 
-# Clean up
-rm -rf _book
-
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
--- _deploy.sh
+++
@@ -1,6 +0,0 @@
-cd book-website
-git rm -rf *
-cp -r ../_book/* ./
-git add --all *
-git commit -m "Update course website"
-git push -q origin gh-pages
